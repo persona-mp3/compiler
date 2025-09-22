9a1439c325301ec0e0e0a0cc7e39da6f8ec0a3b7
persona-mp3 <randomnobscurebs@gmail.com>
2025-09-22 11:45:36 +0100

    dilema

--- lexer.cpp
+++ lexer.cpp
@@ -1,45 +1,33 @@
-/*
- * So we need to be able to get a stream of text
- * and identify what each set of words/character means.
- * For example. int n = 32 * 3
- * The token should be able to say
- *  - int -> reserverd/type
- *  - n -> variable/identifier
- *  - = -> operation/assignment
- *  - 32 -> character/number
- *  - * -> operation
- *  - 3 -> character/number
- *
- * So is thinking about how to structure the code wrong right now??
- * Well not really, but lets see how we might do this.
- *
- * ------------
- *  - First we need to read a stream of characters, for now, we can use std::cin
- *  - Secondly, iterate through the stream of characters and identify them
- * against the table
- *  - For now, we can just print their position
- * */
-
+#include <cstddef>
 #include <iostream>
+#include <map>
 #include <set>
 #include <vector>
+const char ENDLN = ';';
+const char EXP = '^';
+const char OPEN_QUOTE = '"';
+const char CLOSING_QUOTE = '"';
 const std::string OR = "||";
 const std::string INT = "int";
 const std::string STRING = "string";
 const std::string BOOL = "bool";
 const std::string ARR = "arr";
-const char ENDLN = ';';
-const char EXP = '^';
 
 const std::set<std::string> RESERVERD = {OR, INT, STRING, BOOL, ARR};
+const std::set<char> PUNCTUATIONS = {OPEN_QUOTE, CLOSING_QUOTE};
 const std::set<char> OPERATIONS = {ENDLN, EXP};
+const std::set<std::string> TYPE_SET = {STRING, INT, BOOL, ARR};
+
+// so instead of a set, we can just create a map that tells them where they're
+// from
+std::map<std::string, int> TYPES;
 
 std::vector<std::string> splint(std::string s) {
   char delimiter = ' ';
   std::vector<std::string> result;
   std::string word = "";
   for (int i = 0; i < s.length(); i++) {
-    if (s[i] == delimiter) {
+    if (s[i] == delimiter && word != "") {
       result.push_back(word);
       word = "";
     } else if (s[i] == ENDLN) {
@@ -53,16 +41,13 @@ std::vector<std::string> splint(std::string s) {
     result.push_back(word);
   }
 
+  result.push_back(";");
   return result;
 }
 void tokenIterator(std::string &stream) {
-  std::vector<std::string> res = splint(stream);
-  for (int i = 0; i < res.size(); i++) {
-    if (RESERVERD.contains(res[i])) {
-      std::cout << "Found reserved -> " << res[i] << " in resreved\n";
-    } else {
-      std::cout << "Unidentified token ->" << res[i] << '\n';
-    }
+  std::vector<std::string> tokens = splint(stream);
+  for (size_t i = 0; i < tokens.size(); i++) {
+    std::cout << "[" << tokens[i] << "]\n";
   }
 }
 
